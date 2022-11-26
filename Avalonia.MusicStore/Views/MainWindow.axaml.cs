d40a8f7e048be3ac2d2590a94090dbac11aa7aa9
POMXARK <1997pom@gmail.com>
2022-11-26 13:23:32 +1000

    Add project files.

---
+++ Avalonia.MusicStore/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Avalonia.MusicStore.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
