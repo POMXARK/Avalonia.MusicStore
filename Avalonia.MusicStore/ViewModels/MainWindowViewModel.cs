9b44c90d979cd266b26a7ce5e851cd038b44570d
POMXARK <1997pom@gmail.com>
2022-11-26 20:13:16 +1000

    Add Items to Users Collection

--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.Collections.ObjectModel;
 using System.Reactive.Linq;
 using System.Windows.Input;
+using System;
 
 namespace Avalonia.MusicStore.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        [Reactive] bool CollectionEmpty { get; set; }
+
         public MainWindowViewModel()
         {
             ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
@@ -15,9 +20,19 @@ namespace Avalonia.MusicStore.ViewModels
                 var store = new MusicStoreViewModel();
 
                 var result = await ShowDialog.Handle(store);
+
+                if (result != null)
+                {
+                    Albums.Add(result);
+                }
             });
+
+            this.WhenAnyValue(x => x.Albums.Count)
+                .Subscribe(x => CollectionEmpty = x == 0);
         }
+
         public ICommand BuyMusicCommand { get; }
         public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+        public ObservableCollection<AlbumViewModel> Albums { get; } = new();
     }      
 }
